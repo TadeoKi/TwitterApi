1b47fbfcd41f42a5b9a3e1cc3002556d324910cd
TadeoKi <127800278+TadeoKi@users.noreply.github.com>
2023-05-17 20:21:50 -0300

    Initial commit

---
+++ src/Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace TwitterUCU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var twitter = new TwitterImage();
+            string path = File.Exists(@"../../bill2.jpg") ? @"../../bill2.jpg" : @"bill2.jpg";
+            Console.WriteLine(twitter.PublishToTwitter("New Employee 2! ", path));
+            var twitterDirectMessage = new TwitterMessage();
+            Console.WriteLine(twitterDirectMessage.SendMessage("Hola!", "1396065818"));
+        }
+    }
+}
